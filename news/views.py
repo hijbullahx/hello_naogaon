fde38541bb0affdaa397853c125614698f9567f7
hijbullahx <hijbullah119445@gmail.com>
2026-06-04 11:26:57 +0600

    frontend part inititations

--- news/views.py
+++ news/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+def news_list(request):
+    articles = Article.objects.filter(is_published=True).order_by('-publish_date')
+    return render(request, 'news/news_list.html', {'articles': articles})
+
+def news_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk, is_published=True)
+    return render(request, 'news/news_detail.html', {'article': article})
